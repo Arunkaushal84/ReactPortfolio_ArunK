f00030e47da972acca9aa391c40881b43c4b487a
Arun Kaushal <arunkaushal84@gmail.com>
2023-08-15 20:22:06 -0400

    added files

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -6,13 +6,13 @@ export default function footer() {
        <footer className='fixed bottom-0 left-0 right-0  p-6 bg-slate-500 text-center'>
          {/* social media icons  */}
          <div className="flex space-x-4 justify-center sm:justify-center">
-              <a href="https://github.com/Amritpal-Kaur0">
+              <a href="https://github.com/Arunkaushal84">
                 <i className=" p-4 uil uil-github "></i>
               </a>
-              <a href="https://www.linkedin.com/in/amritpal-kaur-a3478a121/">
+              <a href="https://www.linkedin.com/feed/?trk=homepage-basic_sign-in-submit">
                 <i className="uil uil-linkedin p-4"></i>
               </a>
-              <a href="#facebook">
+              <a href="https://www.facebook.com/kaushal.arun84">
                 <i className="uil uil-facebook-f p-4"></i>
               </a>
               <a href="#twitter">
